5545ffe5942ba64744af4a36d26b43f0ebf1b21f
Ramazan <laumanov.r@gmail.com>
2020-02-16 22:27:33 +0600

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+        <div class="info">
+            <div class="user" v-if="currentUser">
+                <div class="name">Здравствуйте {{ currentUser.name }},</div>
+                <div class="logout" @click="$emit('logout')">Выйти</div>
+            </div>
+            <div class="login" v-else>
+                <router-link :to="{name: 'login'}">Войти</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'post-header',
+        props: ['currentUser'],
+        data() {
+            return {}
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
