fd6605f1095a423bc181488d35f3f7e7545f56ad
Ramazan <laumanov.r@gmail.com>
2020-02-16 22:31:56 +0600

    modified main file of imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/style/main.scss';
+import router from './router';
+import VueMaterial from 'vue-material';
+import Toaster from 'v-toaster';
+import JwPagination from 'jw-vue-pagination';
 
-Vue.config.productionTip = false
+
+Vue.use(VueMaterial);
+Vue.use(Toaster);
+Vue.component('jw-pagination', JwPagination);
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
